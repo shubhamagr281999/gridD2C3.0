c1e3e2229673047deb0409f895f985d86b317199
Amit Meena <amitmeena21082001@gmail.com>
2021-07-19 23:17:37 +0530

    added default data

--- software/data/robot.py
+++ software/data/robot.py
@@ -1,1 +1,19 @@
-print("hello world!")
+class Position:
+    def __init__(self,x,y,ori):
+        self.x=x
+        self.y=y
+        self.ori=ori
+
+class Velocity:
+    def __init__(self,wl,wr,v):
+        self.wl=wl
+        self.wr=wr
+        self.v=v
+
+
+class Robot(Position,Velocity):
+    def __init__(self,x,y,ori,wl,wr,v,name):
+        Position.__init__(self,x,y,ori)
+        Velocity.__init__(self,wl,wr,v)
+        self.name=name
+    
