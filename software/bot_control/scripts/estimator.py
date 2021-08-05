929de0770b9ec568aa68602067a87e2fe8e2c89b
Shubham <shubhamagr281999@gmail.com>
2021-08-05 23:19:48 +0530

    aruco detection completed

---
+++ software/bot_control/scripts/estimator.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+import rospy
+import cv2
+from math import sqrt, atan
+from sensor_msgs.msg import Image
+import numpy as np
+from cv_bridge import CvBridge, CvBridgeError
+from geometry_msgs.msg import Pose2D
+
+class pose_publisher:
+    def __init__(self):
+        self.current_pose=np.empty((4,3))
+        self.bot1_pose=Pose2D()
+        self.bot2_pose=Pose2D()
+        self.bot3_pose=Pose2D()
+        self.bot4_pose=Pose2D()
+        self.factor=0.0019704433497537
+        self.pub_bot1_pose=rospy.Publisher('/bot1_pose',Pose2D,queue_size=10)
+        self.pub_bot2_pose=rospy.Publisher('/bot2_pose',Pose2D,queue_size=10)
+        self.pub_bot3_pose=rospy.Publisher('/bot3_pose',Pose2D,queue_size=10)
+        self.pub_bot4_pose=rospy.Publisher('/bot4_pose',Pose2D,queue_size=10)
+        self.camera_sub=rospy.Subscriber("/arena/arena1/camera1/image_raw", Image, self.callback_opencv)
+    def loop(self):
+        rospy.loginfo('reading from camera')
+        rospy.spin()
+    def callback_opencv(self,data):
+        bridge = CvBridge()
+        img = bridge.imgmsg_to_cv2(data, "bgr8")
+        arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_7X7_50)
+        arucoParams = cv2.aruco.DetectorParameters_create()
+        (corners, ids, rejected) = cv2.aruco.detectMarkers(img, arucoDict,parameters=arucoParams)
+
+        #now starting to localise bot wrt to the ids
+        a=np.where(ids==10)
+        if a[0].size==1:
+            (topLeft, topRight, bottomRight, bottomLeft) = corners[a[0][0]][0]
+            topRight = (int(topRight[0]), int(topRight[1]))
+            bottomRight = (float(bottomRight[0]), float(bottomRight[1]))
+            bottomLeft = (float(bottomLeft[0]), float(bottomLeft[1]))
+            topLeft = (int(topLeft[0]), int(topLeft[1]))
+            cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+            cY = int((topLeft[1] + bottomRight[1]) / 2.0)   
+            self.current_pose[0][1]=-(cX-img.shape[1]/2.0)*self.factor
+            self.current_pose[0][0]=-(cY-img.shape[0]/2.0)*self.factor
+            self.current_pose[0][2]=atan((bottomLeft[0]- bottomRight[0])/(bottomLeft[1]- bottomRight[1]))
+            print(self.current_pose[0][0],self.current_pose[0][1],self.current_pose[0][2])
+        a=np.where(ids==20)
+        if a[0].size==1:
+            (topLeft, topRight, bottomRight, bottomLeft) = corners[a[0][0]][0]
+            topRight = (int(topRight[0]), int(topRight[1]))
+            bottomRight = (float(bottomRight[0]), float(bottomRight[1]))
+            bottomLeft = (float(bottomLeft[0]), float(bottomLeft[1]))
+            topLeft = (int(topLeft[0]), int(topLeft[1]))
+            cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+            cY = int((topLeft[1] + bottomRight[1]) / 2.0)        
+            self.current_pose[1][1]=-(cX-img.shape[1]/2.0)*self.factor
+            self.current_pose[1][0]=-(cY-img.shape[0]/2.0)*self.factor
+            self.current_pose[1][2]=atan((bottomLeft[0]- bottomRight[0])/(bottomLeft[1]- bottomRight[1]))
+        a=np.where(ids==30)
+        if a[0].size==1:
+            (topLeft, topRight, bottomRight, bottomLeft) = corners[a[0][0]][0]
+            topRight = (int(topRight[0]), int(topRight[1]))
+            bottomRight = (float(bottomRight[0]), float(bottomRight[1]))
+            bottomLeft = (float(bottomLeft[0]), float(bottomLeft[1]))
+            topLeft = (int(topLeft[0]), int(topLeft[1]))
+            cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+            cY = int((topLeft[1] + bottomRight[1]) / 2.0)     
+            self.current_pose[2][1]=-(cX-img.shape[1]/2.0)*self.factor
+            self.current_pose[2][0]=-(cY-img.shape[0]/2.0)*self.factor
+            self.current_pose[2][2]=atan((bottomLeft[0]- bottomRight[0])/(bottomLeft[1]- bottomRight[1]))
+        a=np.where(ids==40)
+        if a[0].size==1:
+            (topLeft, topRight, bottomRight, bottomLeft) = corners[a[0][0]][0]
+            topRight = (int(topRight[0]), int(topRight[1]))
+            bottomRight = (float(bottomRight[0]), float(bottomRight[1]))
+            bottomLeft = (float(bottomLeft[0]), float(bottomLeft[1]))
+            topLeft = (int(topLeft[0]), int(topLeft[1]))
+            cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+            cY = int((topLeft[1] + bottomRight[1]) / 2.0)     
+            self.current_pose[3][1]=-(cX-img.shape[1]/2.0)*self.factor
+            self.current_pose[3][0]=-(cY-img.shape[0]/2.0)*self.factor
+            self.current_pose[3][2]=atan((bottomLeft[0]- bottomRight[0])/(bottomLeft[1]- bottomRight[1]))
+        self.bot1_pose.x=self.current_pose[0][0]
+        self.bot1_pose.y=self.current_pose[0][1]
+        self.bot1_pose.theta=self.current_pose[0][2] 
+        self.pub_bot1_pose.publish(self.bot1_pose)
+
+        self.bot2_pose.x=self.current_pose[1][0]
+        self.bot2_pose.y=self.current_pose[1][1]
+        self.bot2_pose.theta=self.current_pose[1][2]
+        self.pub_bot2_pose.publish(self.bot2_pose)
+
+        self.bot3_pose.x=self.current_pose[2][0]
+        self.bot3_pose.y=self.current_pose[2][1]
+        self.bot3_pose.theta=self.current_pose[2][2]
+        self.pub_bot3_pose.publish(self.bot3_pose)
+
+        self.bot4_pose.x=self.current_pose[3][0]
+        self.bot4_pose.y=self.current_pose[3][1]
+        self.bot4_pose.theta=self.current_pose[3][2]
+        self.pub_bot4_pose.publish(self.bot4_pose)
+
+if __name__ == '__main__':
+    rospy.init_node('pose_estimator')
+    pose_pub_obj=pose_publisher()
+    pose_pub_obj.loop()
--- software/bot_perception/scripts/scanner.py
+++ software/bot_perception/scripts/scanner.py
@@ -1,34 +1,27 @@
 #!/usr/bin/env python
 
 import rospy
+import imutils
 from sensor_msgs.msg import Image
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 from math import sqrt
-from std_msgs.msg import Int16
 from cv_bridge import CvBridge, CvBridgeError
-from geometry_msgs.msg import PoseStamped, Pose2D
+from geometry_msgs.msg import Pose2D
 
 def callback_opencv(data):
 	bridge = CvBridge()
 	img = bridge.imgmsg_to_cv2(data, "bgr8")
-
 	arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_7X7_50)
 	arucoParams = cv2.aruco.DetectorParameters_create()
-		 
-	img = cv2.medianBlur(img,3)
 	(corners, ids, rejected) = cv2.aruco.detectMarkers(img, arucoDict,parameters=arucoParams)
+
+	#now starting to localise bot wrt to the ids
 	print(ids)
+	while ()
 	cX = 0
 	cY = 0
-
-	#cv2.circle(img,(img.shape[1]//2,img.shape[0]//2),4,(255,0,0),-1)
-
-	# aruco = Pose2D()
-	# aruco.x = 0.0
-	# aruco.y = 0.0
-	# aruco.theta = 0.0
 
 if __name__ == '__main__':
 
