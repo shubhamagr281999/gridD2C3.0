eb2a234ae5e7ea70d8e3d40952276367afbd686f
Shubham <shubhamagr281999@gmail.com>
2021-08-08 12:06:04 +0530

    added controllers files which are still being developed and corrected pose_estimator for detecting correct angle

--- software/bot_control/scripts/estimator.py
+++ software/bot_control/scripts/estimator.py
@@ -2,7 +2,7 @@
 
 import rospy
 import cv2
-from math import sqrt, atan
+from math import sqrt, atan, pi
 from sensor_msgs.msg import Image
 import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
@@ -21,9 +21,18 @@ class pose_publisher:
         self.pub_bot3_pose=rospy.Publisher('/bot3_pose',Pose2D,queue_size=10)
         self.pub_bot4_pose=rospy.Publisher('/bot4_pose',Pose2D,queue_size=10)
         self.camera_sub=rospy.Subscriber("/arena/arena1/camera1/image_raw", Image, self.callback_opencv)
-    def loop(self):
-        rospy.loginfo('reading from camera')
-        rospy.spin()
+
+    def angle(self,y,x):
+        x=x+0.000001
+        if(x>0 and y>0):
+            return atan(y/x)
+        elif(x<0 and y>0):
+            return (pi +atan(y/x))
+        elif(x<0 and y<0):
+            return (atan(y/x)-pi)
+        else:
+            return atan(y/x)
+
     def callback_opencv(self,data):
         bridge = CvBridge()
         img = bridge.imgmsg_to_cv2(data, "bgr8")
@@ -39,12 +48,14 @@ class pose_publisher:
             bottomRight = (float(bottomRight[0]), float(bottomRight[1]))
             bottomLeft = (float(bottomLeft[0]), float(bottomLeft[1]))
             topLeft = (int(topLeft[0]), int(topLeft[1]))
+            # print(bottomRight,bottomLeft)
             cX = int((topLeft[0] + bottomRight[0]) / 2.0)
             cY = int((topLeft[1] + bottomRight[1]) / 2.0)   
             self.current_pose[0][1]=-(cX-img.shape[1]/2.0)*self.factor
             self.current_pose[0][0]=-(cY-img.shape[0]/2.0)*self.factor
-            self.current_pose[0][2]=atan((bottomLeft[0]- bottomRight[0])/(bottomLeft[1]- bottomRight[1]))
-            print(self.current_pose[0][0],self.current_pose[0][1],self.current_pose[0][2])
+            self.current_pose[0][2]=self.angle((bottomRight[0]- bottomLeft[0]),(bottomRight[1]- bottomLeft[1]))
+            
+
         a=np.where(ids==20)
         if a[0].size==1:
             (topLeft, topRight, bottomRight, bottomLeft) = corners[a[0][0]][0]
@@ -56,7 +67,8 @@ class pose_publisher:
             cY = int((topLeft[1] + bottomRight[1]) / 2.0)        
             self.current_pose[1][1]=-(cX-img.shape[1]/2.0)*self.factor
             self.current_pose[1][0]=-(cY-img.shape[0]/2.0)*self.factor
-            self.current_pose[1][2]=atan((bottomLeft[0]- bottomRight[0])/(bottomLeft[1]- bottomRight[1]))
+            self.current_pose[1][2]=self.angle((bottomRight[0]- bottomLeft[0]),(bottomRight[1]- bottomLeft[1]))
+
         a=np.where(ids==30)
         if a[0].size==1:
             (topLeft, topRight, bottomRight, bottomLeft) = corners[a[0][0]][0]
@@ -68,7 +80,8 @@ class pose_publisher:
             cY = int((topLeft[1] + bottomRight[1]) / 2.0)     
             self.current_pose[2][1]=-(cX-img.shape[1]/2.0)*self.factor
             self.current_pose[2][0]=-(cY-img.shape[0]/2.0)*self.factor
-            self.current_pose[2][2]=atan((bottomLeft[0]- bottomRight[0])/(bottomLeft[1]- bottomRight[1]))
+            self.current_pose[2][2]=self.angle((bottomRight[0]- bottomLeft[0]),(bottomRight[1]- bottomLeft[1]))
+            
         a=np.where(ids==40)
         if a[0].size==1:
             (topLeft, topRight, bottomRight, bottomLeft) = corners[a[0][0]][0]
@@ -80,7 +93,8 @@ class pose_publisher:
             cY = int((topLeft[1] + bottomRight[1]) / 2.0)     
             self.current_pose[3][1]=-(cX-img.shape[1]/2.0)*self.factor
             self.current_pose[3][0]=-(cY-img.shape[0]/2.0)*self.factor
-            self.current_pose[3][2]=atan((bottomLeft[0]- bottomRight[0])/(bottomLeft[1]- bottomRight[1]))
+            self.current_pose[3][2]=self.angle((bottomRight[0]- bottomLeft[0]),(bottomRight[1]- bottomLeft[1]))
+
         self.bot1_pose.x=self.current_pose[0][0]
         self.bot1_pose.y=self.current_pose[0][1]
         self.bot1_pose.theta=self.current_pose[0][2] 
@@ -103,5 +117,7 @@ class pose_publisher:
 
 if __name__ == '__main__':
     rospy.init_node('pose_estimator')
+    rospy.loginfo('reading from camera')
     pose_pub_obj=pose_publisher()
-    pose_pub_obj.loop()
+    #to keep the function running in loop
+    rospy.spin()
